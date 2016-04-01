732a4ca915159e497f4d023e0e2d6121f43475ad
Paul-Vincent Roll <paviro@me.com>
2016-04-01 13:40:14 +0200

    Added module swapping

--- MMM-Facial-Recognition.js
+++ MMM-Facial-Recognition.js
@@ -26,24 +26,64 @@ Module.register('MMM-Facial-Recognition',{
 		// Logout delay after last recognition so that a user does not get instantly logged out if he turns away from the mirror for a few seconds
 		LOGOUT_DELAY: 15,
 		// Array with usernames (copy and paste from training script)
-		USER: []
+		USER: [],
+		//Module set used for strangers and if no user is detected
+		DEFAULT_CLASS: "default",
+		//Set of modules which should be shown for every user
+		FOR_ALL: "for_all"
+		
 	},
 	
 	// Override socket notification handler.
 	socketNotificationReceived: function(notification, payload) {
 		if (payload.action == "login"){
-			console.log(payload.user);
 			if (payload.user == -1){
 				this.current_user = "stranger"
 			}
-			else{
+			else{				
 				this.current_user = this.config.USER[payload.user];
+				
+				MM.getModules().withClass(this.config.DEFAULT_CLASS).exceptWithClass(this.config.FOR_ALL).enumerate(function(module) {
+					module.hide(1000, function() {
+						Log.log(module.name + ' is hidden.');
+					});
+				});
+				
+				MM.getModules().withClass(this.current_user).enumerate(function(module) {
+					module.show(1000, function() {
+						Log.log(module.name + ' is shown.');
+					});
+				});
 			}
 			this.updateDom(3000);
 		}
 		else if (payload.action == "logout"){
+		
+			MM.getModules().withClass(this.current_user).enumerate(function(module) {
+				module.hide(1000, function() {
+					Log.log(module.name + ' is hidden.');
+				});
+			});
+			
+			MM.getModules().withClass(this.config.DEFAULT_CLASS).exceptWithClass(this.config.FOR_ALL).enumerate(function(module) {
+				module.show(1000, function() {
+					Log.log(module.name + ' is shown.');
+				});
+			});
+			
 			this.current_user = null;
 			this.updateDom(3000);
+		}
+	},
+	
+	notificationReceived: function(notification, payload, sender) {
+		if (notification === 'DOM_OBJECTS_CREATED') {
+			MM.getModules().exceptWithClass("default").enumerate(function(module) {
+				console.log(module);
+				module.hide(1000, function() {
+					Log.log('Module is hidden.');
+				});
+			});
 		}
 	},
 	
@@ -55,7 +95,7 @@ Module.register('MMM-Facial-Recognition',{
 	
 	getDom: function() {
 		var wrapper = document.createElement("div");
-		wrapper.className = "normal light"
+		wrapper.className = "small light"
 		if (this.current_user != null){
 		wrapper.innerHTML = "Hello " + this.current_user +  ". Nice to see you!"
 
--- README.md
+++ README.md
@@ -4,7 +4,7 @@ This an extension for the [MagicMirror](https://github.com/MichMich/MagicMirror)
 ## Usage
 To train the needed model use the [MMM-Facial-Recognition-Tools](https://github.com/paviro/MMM-Facial-Recognition-Tools).
 
-The entry in config.js can look like the following. (NOTE: You only have to add a variables if want to change its standard value.)
+The entry in config.js can look like the following. (NOTE: You only have to add the variables to config if want to change its standard value.)
 
 ```
 {
@@ -27,13 +27,32 @@ The entry in config.js can look like the following. (NOTE: You only have to add 
 		// Logout delay after last recognition so that a user does not get instantly logged out if he turns away from the mirror for a few seconds
 		LOGOUT_DELAY: 15,
 		// Array with usernames (copy and paste from training script)
-		USER: []
+		USER: [],
+		//Module set used for strangers and if no user is detected
+		DEFAULT_CLASS: "default",
+		//Set of modules which should be shown for every user
+		FOR_ALL: "for_all"
 	}
 }
 ```
 
-## Important Notes
-- Currently this only displays who is recognised, as soon as module swaping is supported by the mirror system it will be added.
+In order for this module to do anything useful you have to assign custom classes to your modules. The class `default` (if you don't change it) is shown if no user is detected or a stranger. The class `for_all` (if you don't change it) is shown for all users. To specify modules for a certain user, use their name as classname.
+
+```
+{
+	module: 'example_module',
+	position: 'top_left',
+	//Set your classes here seperated by a space.
+	//Shown for all users
+	classes: 'default for_all'
+},
+{
+	module: 'example_module2',
+	position: 'top_left',
+	//Only shown for me
+	classes: 'Paul-Vincent'
+}
+```
 
 ## Dependencies
 - [OpenCV](http://opencv.org) (sudo apt-get install libopencv-dev python-opencv)
