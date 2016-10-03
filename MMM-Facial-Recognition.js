58b65d7fb61ed22bef9cc30cfffcf15389934648
Paul-Vincent Roll <paviro@me.com>
2016-10-03 14:34:28 +0200

    Fixed user logout broadcast

--- MMM-Facial-Recognition.js
+++ MMM-Facial-Recognition.js
@@ -75,7 +75,7 @@ Module.register('MMM-Facial-Recognition',{
 			});
 		});
 		
-		this.sendNotification("CURRENT_USER", this.current_user);
+		this.sendNotification("CURRENT_USER", "None");
 	},
 	
 	// Override socket notification handler.
